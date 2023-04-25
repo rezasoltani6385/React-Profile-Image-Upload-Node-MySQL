40b66e39bcdd8e20c9f2a4fcce9595c20603e60c
Reza Soltani <reza.soltani@gmail.com>
2023-04-25 12:36:32 +0330

    some necessary midification for better experience

--- client/src/Page/UploadAvatar.js
+++ client/src/Page/UploadAvatar.js
@@ -7,15 +7,20 @@ import { Base_URL } from "../Components/BaseURL";
 
 
 
-function UploadAvatar() {
+function UploadAvatar(props) {
+  const userId = props.userId
   const editorRef = useRef(null);
   const [scale, setScale] = useState(1)
   const [img, setImg] = useState(null)
-  const userId = 8
+  const [uploadStatus, setUploadStatus] = useState(false)
+
 
   const handleFileChange = (evt) => {
     const file = evt.target.files[0];
     if (file) {
+      document.getElementById('avatarEditor').style.display = 'flex'
+      document.getElementById('controls').style.display = 'flex'
+      document.getElementById('uploadBtn').style.display = 'flex'
       const reader = new FileReader();
       reader.onload = (event) => {
         setImg(event.target.result);
@@ -64,8 +69,12 @@ function UploadAvatar() {
 
             const imageData = canvas.toDataURL();
 
-            
-            axios.post(`${Base_URL}/upload`, { imageData, userId }) // send the image data to the server
+            // send the image data to the server
+            axios.post(`${Base_URL}/upload`, { imageData, userId })
+            .then((response)=>{if (response.status === 200){
+              setUploadStatus(true)
+            }})
+            .catch((error)=>{document.getElementById('error').innerHTML = error.message}) 
         
         }
     }
@@ -74,35 +83,52 @@ function UploadAvatar() {
 
   return (
     <div>
-        <div >
+      {!uploadStatus ?
+        <div>
+
+          <p id='error' className="d-flex justify-content-center" style={{color: 'red', fontSize: '18px'}}></p>
+          <div id="avatarEditor"  style={{display: 'none', justifyContent: 'center'}}>
             <AvatarEditor
-                ref={editorRef}
-                image={img}
-                width={300}
-                height={300}
-                border={10}
-                borderRadius={180}
-                color={[245, 245, 245, 1.5]} // RGBA
-                scale={scale}
+              ref={editorRef}
+              image={img}
+              width={300}
+              height={300}
+              border={10}
+              borderRadius={180}
+              color={[245, 245, 245, 1.5]} // RGBA
+              scale={scale}
             />
+          </div>
+          <br />
+          <div id="controls" style={{display: 'none', justifyContent: 'center'}}>
+            <button className="btn btn-primary rounded-pill" style={{marginLeft: '10px', marginRight: '10px'}} onClick={()=> 2 > scale > 0 && setScale(scale + 0.1)}>
+                <FontAwesomeIcon icon={faSearchPlus} />
+            </button>
+            <button className="btn btn-primary rounded-pill" style={{marginLeft: '10px', marginRight: '10px',}} onClick={()=> scale > 0.6 && setScale(scale - 0.1)}> 
+                <FontAwesomeIcon icon={faSearchMinus} />
+            </button>
+            <button className="btn btn-primary rounded-pill" style={{marginLeft: '10px', marginRight: '10px',}} onClick={()=> scale !== 1 && setScale(1)}> 
+                <FontAwesomeIcon icon={faSyncAlt} />
+            </button>
+          </div>
+          <br />
+          <div className="d-flex justify-content-center" >
+            <input className="form-control" type='file' accept="image/*" onChange={handleFileChange} />
+          </div>
+          <br />
+          <div id="uploadBtn" style={{display: 'none', justifyContent: 'center'}}>
+            <button className="btn btn-success rounded" onClick={handleSave}>Upload</button>
+          </div>
         </div>
-      <div className="d-flex">
-        <button className="btn btn-primary rounded-pill" style={{marginLeft: '10px', marginRight: '10px'}} onClick={()=> 2 > scale > 0 && setScale(scale + 0.1)}>
-            <FontAwesomeIcon icon={faSearchPlus} />
-        </button>
-        <button className="btn btn-primary rounded-pill" style={{marginLeft: '10px', marginRight: '10px',}} onClick={()=> scale > 0.6 && setScale(scale - 0.1)}> 
-            <FontAwesomeIcon icon={faSearchMinus} />
-        </button>
-        <button className="btn btn-primary rounded-pill" style={{marginLeft: '10px', marginRight: '10px',}} onClick={()=> scale !== 1 && setScale(1)}> 
-            <FontAwesomeIcon icon={faSyncAlt} />
-        </button>
-      </div>
-      <br />
-      <input type='file' accept="image/*" onChange={handleFileChange} />
-      <br />
-      <button onClick={handleSave}>Save</button>
+        :
+          <div className='text-center'>
+              <h3>Upload successful!</h3>
+              <br />
+              <button className='btn btn-success rounded' onClick={() => props.setUploadModal(false)}>Close</button>
+          </div>
+      }
     </div>
-  );
+  )
 }
 
 export default UploadAvatar;
